6a074a2d95b8dce1e74cf841777e8d13e9841eb2
dungchi <kuinochi@gmail.com>
2016-05-07 00:47:53 +0800

    Edit Read header

--- src/main/java/c4Lab/VCFReader.java
+++ src/main/java/c4Lab/VCFReader.java
@@ -6,7 +6,14 @@
 
 package c4Lab;
 
+import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFCodec;
+import htsjdk.tribble.readers.LineIterator;
+import htsjdk.tribble.readers.LineIteratorImpl;
+import htsjdk.tribble.readers.LineReaderUtil;
+import htsjdk.variant.vcf.VCFHeader;
+import htsjdk.variant.variantcontext.Allele;
+
 import java.io.*;
 import java.util.*;
 
@@ -14,12 +21,44 @@ import java.util.*;
 public class VCFReader {
 
 
-    public static void main( String[] args ) throws IOException {
+    public static void main(String[] args) throws IOException {
         long startTime;
         startTime = System.currentTimeMillis();
-        VCFCodec vcfCodec = new VCFCodec();
-        FileWriter out = new FileWriter("");
-        System.out.println(startTime);
 
+        final String inVCF = args[0];
+
+
+        // Read input VCF
+        BufferedReader schemaReader = new BufferedReader(new FileReader(inVCF));
+        VCFCodec vcfCodec = new VCFCodec();
+        String line;
+        String headerLine = "";
+        VariantContext vctx;
+        while ((line = schemaReader.readLine()) != null) {
+
+            // Header line start with comment sign
+            if (line.startsWith("#")) {
+                headerLine = headerLine.concat(line).concat("\n"); // concatenate all the header line
+                continue;
+            }
+
+            //VCFHeader header =;
+            //vcfCodec.readActualHeader(new LineIteratorImpl(LineReaderUtil.fromStringReader(
+            //        new StringReader(headerLine), LineReaderUtil.LineReaderOption.SYNCHRONOUS)));
+
+            /*
+            // Variant is stored line by line
+            if(!line.startsWith("#")) {
+                vctx = vcfCodec.decode(line);
+                if(vctx.getAlternateAlleles().get(0).length()>100)
+                    System.out.println(" length: "+vctx.getAlternateAlleles().get(0).length()+" ref: "+vctx.getReference() +
+                            " alt:" + vctx.getAlternateAlleles().get(0) + " GT: " +
+                            vctx.getGenotype(vctx.getSampleNamesOrderedByName().get(0)).getGenotypeString() );
+
+
+            }
+*/
+        }
+        System.out.print(headerLine);
     }
 }
